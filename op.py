45d20390359401adcce16f00902002d07b278df7
Rakshada19 <rakshadachaudhari@gmail.com>
2025-10-27 19:42:12 +0530

    Create op.py

---
+++ op.py
@@ -0,0 +1,19 @@
+--- Weighted Grid Path-to-Gold A* Solver ---
+Start: (0, 0), Goal: (5, 5)
+Tile Costs (1=Road, 10=Swamp):
+
+[1, 1, 1, 5, 5, 10]
+[1, 5, 1, 1, 5, 10]
+[1, 5, 1, 1, 1, 1]
+[1, 5, 5, 5, 1, 1]
+[1, 1, 1, 1, 1, 1]
+[10, 10, 5, 1, 1, 1]
+--------------------------------------------------
+✅ Path Found!
+Total Minimum Cost (g_score): 10
+Path Length (Steps): 10
+Nodes Explored (Efficiency): 23
+Time Taken: 0.000067 seconds
+
+Optimal Path (Coordinates):
+[(0, 0), (0, 1), (0, 2), (1, 2), (1, 3)] ... [(2, 4), (2, 5), (3, 5), (4, 5), (5, 5)]
